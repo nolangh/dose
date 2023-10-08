8b79c860222903b1289b4910a86cc02204873522
nolangh <nolanghiggins@gmail.com>
2023-10-07 20:48:28 -0600

    -a

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8b88ca4e03ca9d3b"; }
+/******/ 		__webpack_require__.h = function() { return "8431ca0c6fadd7f3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1401,6 +1401,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -2,14 +2,24 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import Link from "next/link";
+import styled from "styled-components";
+
+{
+	/*Styled componenets */
+}
+const Main = styled.main`
+	background-color: darkolivegreen;
+`;
 
 export default function Signup() {
 	return (
-		<main className="container main-test">
-			<button>
-				<Link href="/">Home</Link>
-			</button>
-			Signup
-		</main>
+		<div>
+			<Main className="container-fluid">
+				<button>
+					<Link href="/">Home</Link>
+				</button>
+				Signup
+			</Main>
+		</div>
 	);
 }
