8eefb87361e45557a3c8a7a11ed19b16392b027b
nolangh <nolanghiggins@gmail.com>
2023-10-08 03:17:39 -0600

    -a

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,26 +4,27 @@ import Link from "next/link";
 import styles from "./Home.module.scss";
 
 export default function Home() {
-	return (
-		<main>
-			<h1 className={styles.title}>DOSE</h1>
-			<div>
-				<ul>
-					<li>1</li>
-					<li>2</li>
-					<li>3</li>
-					<li>4</li>
-				</ul>
-			</div>
-			<div className={styles.button_cont}>
-				<Link href="/signup" className={styles.links}>
-					Sign Up
-				</Link>
-				<Link href="./login/" className={styles.links}>
-					Login
-				</Link>
-				{/*End Button sections*/}
-			</div>
-		</main>
-	);
+  return (
+    <main>
+      <h1 className={styles.title}>DOSE</h1>
+      <div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>test for formatting</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </div>
+      <div className={styles.button_cont}>
+        <Link href="/signup" className={styles.links}>
+          Sign Up
+        </Link>
+        <Link href="./login/" className={styles.links}>
+          Login
+        </Link>
+        {/*End Button sections*/}
+      </div>
+    </main>
+  );
 }
